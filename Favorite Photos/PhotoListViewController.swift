e361ff6f99b97ce1865562c0a61efb1ed09bd9c0
Vibha Alangar <alangavr@rose-hulman.edu>
2018-04-30 11:58:32 -0400

    permissions, images, and scoreboard

---
+++ Favorite Photos/PhotoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoListViewController.swift
+//  Favorite Photos
+//
+//  Created by CSSE Department on 4/30/18.
+//  Copyright © 2018 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBAction func takePhoto(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+
+}
